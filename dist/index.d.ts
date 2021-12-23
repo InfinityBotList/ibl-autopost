0bc32a09eefcb4a4b8b03a4ecfb7aec25c9399cf
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-23 03:12:23 -0700

    Delete index.d.ts

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseClient } from './clients/BaseClient';
-import { AutoPostOptions } from './typings';
-export declare function InfinityAutoPoster(auth: string, client: any, options?: AutoPostOptions): BaseClient;
-export { DJSClient } from './clients/DJSClient';
-export { ErisClient } from './clients/ErisClient';
-export { DetritusClient } from './clients/DetritusClient';
-export default InfinityAutoPoster;
